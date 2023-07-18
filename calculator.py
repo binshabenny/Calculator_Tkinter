90cb6ec741075e4aa11747e8b3b292a88c98ce17
Binsha Benny <binshabenny3@gmail.com>
2023-07-18 16:49:17 +0530

    new button label are added

--- calculator.py
+++ calculator.py
@@ -2,4 +2,14 @@ from tkinter import *
 window = Tk()
 window.title("calculator")
 window.geometry("600x400")
+
+window.configure(bg="blue")
+window.attributes()
+#widgets
+lb1 = Label(window,text="learn tkinter python")
+lb1.pack()
+t1 = Entry(window)
+t1.pack()
+btn = Button(window,text="click me")
+btn.pack()
 window.mainloop()
